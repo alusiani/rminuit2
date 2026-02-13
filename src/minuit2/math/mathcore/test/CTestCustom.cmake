375e21175008e2a1b13dda8c962c274a2488187a
Alberto Lusiani <alusiani@gmail.com>
2026-02-13 18:20:10 +0100

    cleanup unneeded files in Minuit2

--- src/minuit2/math/mathcore/test/CTestCustom.cmake
+++
@@ -1,6 +0,0 @@
-#---Custom CTest settings---------------------------------------------------
-
-if (CTEST_BUILD_NAME MATCHES aarch64 AND CTEST_BUILD_NAME MATCHES dbg)
-  # these tests are disabled as they timeout
-  list(APPEND CTEST_CUSTOM_TESTS_IGNORE mathcore-testMathRandom)
-endif()
